74e7e378539960ef811ceb575146f30057195d9f
V.V.G <76651541+Vlad-Veresciaghin@users.noreply.github.com>
2021-01-22 15:49:28 +0100

    Update Guess_nr.py

--- Guess_nr.py
+++ Guess_nr.py
@@ -1,15 +1,16 @@
 import random
-# != means DOES NOT EQUAL
+
 def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 and {x}: '))
         if guess < random_number:
-           print('Guess is too low, please try again!')
+            print('Sorry, guess again. Too low.')
         elif guess > random_number:
-           print('Guess is too high, please try again!')
-    print(f'Good job, you guessed right! The number was {random_number}.')
+            print('Sorry, guess again. Too high.')
+
+    print(f'Yay, congrats. You have guessed the number {random_number} correctly!!')
 
 def computer_guess(x):
     low = 1
@@ -19,17 +20,20 @@ def computer_guess(x):
         if low != high:
             guess = random.randint(low, high)
         else:
-            guess = low
-        feedback = input(f'Is my {guess} too high (H), too low (L), or correct(C)? ').lower()
+            guess = low  # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
         if feedback == 'h':
-            high= guess - 1
+            high = guess - 1
         elif feedback == 'l':
-            low == guess + 1
+            low = guess + 1
 
-    print(f'Well done! You are right, the number is {guess} ! ')
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')
 
+# guess = (10) in order to make the computer generate e ramdom nr for you to guess
+# computer_guess = (10) now you have to think of a nr which the computer will try to guess
+# instead of (10) it can be any number
 
-computer_guess(10)
+computer_guess(10) #make sure to use lowercase (ex. h,l,c)
 
 
 
